26699d91a249a8e867af27a91257132521d536b5
mlaxwong <mlaxwong@gmail.com>
2020-11-09 14:45:05 +0800

    first commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+phpinfo();
+// require dirname(__DIR__) . '/vendor/autoload.php';
+
+// $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+// $dotenv->load();
+
+// var_dump($_ENV);
+
+// $pdo = new PDO($_ENV['DB_DNS'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+// $pdotest = new PDO($_ENV['TEST_DB_DNS'], $_ENV['TEST_DB_USER'], $_ENV['TEST_DB_PASSWORD']);
